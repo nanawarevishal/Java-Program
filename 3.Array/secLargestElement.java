559ca8c2f0d407028a5d0026b1e251342ea8be30
nanawarevishal <nanawarevishal18@gmail.com>
2023-04-23 17:06:12 +0530

    commit

--- 3.Array/secLargestElement.java
+++ 3.Array/secLargestElement.java
@@ -1,12 +1,29 @@
+
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class secLargestElement {
-    public static void main(String[] args) {
-        int a[] ={1,35,12,3,34};
+    public static void main(String[] args)throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println("Enter the size of the array: ");
+        int size = Integer.parseInt(br.readLine());
+
+        int a[] = new int[size];
+
+        System.out.println("Enter the array element: ");
+        for(int i=0;i<size;i++)
+        {
+            a[i] = Integer.parseInt(br.readLine());
+
+        }
 
         int max = a[0];
 
         for(int i=0;i<a.length;i++)
         {
-            if(max < a[i])
+            if(max <= a[i])
             {
                 max =a[i];
             }
@@ -20,7 +37,7 @@ public class secLargestElement {
            
             if(a[i] != max)
             {
-                if(secmax < a[i])
+                if(secmax <= a[i])
                 {
                     secmax = a[i];
                 }
