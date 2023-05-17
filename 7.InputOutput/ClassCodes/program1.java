9ee7e917547a42f0e55e20a93e82bf6c90a9a060
nanawarevishal <nanawarevishal18@gmail.com>
2023-05-17 22:11:06 +0530

    main

---
+++ 7.InputOutput/ClassCodes/program1.java
@@ -0,0 +1,16 @@
+
+package ClassCodes;
+
+import java.util.Scanner;
+
+public class program1{
+    public static void main(String[] args) {
+       
+        Scanner obj = new Scanner(System.in);
+
+        System.out.println("Enter your name: ");
+        String name= obj.nextLine();
+
+        System.out.println(name);
+    }
+}
---
+++ 7.InputOutput/ClassCodes/program2.java
@@ -0,0 +1,7 @@
+package ClassCodes;
+
+public class program2 {
+    public static void main(String[] args) {
+        System.out.println("he");
+    }
+}
