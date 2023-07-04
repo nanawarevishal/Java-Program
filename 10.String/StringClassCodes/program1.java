d6b5094b4e238cfe91b25d1df8e7f684bfde68b5
nanawarevishal <nanawarevishal18@gmail.com>
2023-07-04 10:02:04 +0530

    main

--- 10.String/StringClassCodes/program1.java
+++ 10.String/StringClassCodes/program1.java
@@ -2,7 +2,34 @@ package StringClassCodes;
 
 public class program1 {
     public static void main(String[] args) {
+       
+        String str1 = "abc";
+        String str2 = "pqr";
+
+        char ch1[] = str1.toCharArray();
+        char ch2[] = str2.toCharArray();
+
+        char ch3[] = new char[ch1.length+ch2.length];
+
+        for(int i=0;i<ch1.length;i++){
+            ch3[i] = ch1[i];
+        }
+
+        int x=0;
+        for(int j=ch1.length;j<ch3.length;j++){
+            ch3[j]= ch2[x];
+            x++;
+        }
+
+        for(int i=0;i<ch3.length;i++){
+            System.out.println(ch3[i]);
+        }
+
+        String str3  = ch3.toString();
+        System.out.println(str3);
+
+        System.out.println(ch3);
+
         
-        System.out.println("he");
     }
 }
---
+++ 10.String/StringClassCodes/program2.java
@@ -0,0 +1,26 @@
+package StringClassCodes;
+
+public class program2 {
+    public static void main(String[] args) {
+        String str1 = "Core2Web";                           // String Constant Pool
+        String str2 = new String("Core2Web");      // Heap
+        char ch[]={'C','2','W'};                            // Heap 
+        String str3 = new String(ch);
+
+        System.out.println(System.identityHashCode(str1));
+        System.out.println(System.identityHashCode(str2));
+        System.out.println(System.identityHashCode(str3));
+
+
+        String str4 = "Core2Web";
+        String str5 = new String("Core2Web");
+
+        //Identity HashCode
+
+        
+        System.out.println(System.identityHashCode(str4));
+        System.out.println(System.identityHashCode(str5));
+        System.out.println(System.identityHashCode(ch));
+
+    }
+}
