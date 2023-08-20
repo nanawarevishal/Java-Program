090c00db8806fb6bcd124d3a962d50a684cb874c
nanawarevishal <nanawarevishal18@gmail.com>
2023-08-20 10:22:37 +0530

    main

---
+++ 16.ExceptionHandling/RunTime/1_NumberFormatException.java
@@ -0,0 +1,15 @@
+package RunTime;
+
+import java.io.BufferedReader;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class NumberFormat{
+    
+    public static void main(String[] args)throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+
+    }
+}
