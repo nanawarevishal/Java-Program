034902bb851510e73ef08e5e62257e8587319d5e
nanawarevishal <nanawarevishal18@gmail.com>
2023-09-10 20:42:05 +0530

    Linked List

---
+++ 19.Collection/collection/list/LinkedList/1_Linkedls.java
@@ -0,0 +1,20 @@
+package collection.list.LinkedList;
+
+import java.util.LinkedList;
+
+class Linkedls1{
+
+    public static void main(String[] args) {
+        
+        LinkedList ls = new LinkedList<>();
+
+        ls.add(10);
+        ls.addFirst(5);
+        ls.addLast(30);
+        ls.addFirst(2);
+        
+        System.out.println(ls);
+
+
+    }
+}
