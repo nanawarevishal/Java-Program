08fea4021e3b7d0780edae7002ee77e131fb548c
nanawarevishal <nanawarevishal18@gmail.com>
2023-09-10 20:41:49 +0530

    ArrayList

---
+++ 19.Collection/collection/list/arrayList/2_ArrayLs.java
@@ -0,0 +1,17 @@
+package collection.list.arrayList;
+
+import java.util.ArrayList;
+class Arraylst{
+
+    public static void main(String[] args) {
+
+        ArrayList ls = new ArrayList();
+
+        ls.add(10);
+        ls.add(10);
+        ls.add(new Integer(50));
+        ls.add(10);
+
+        System.out.println(ls);
+    }
+}
