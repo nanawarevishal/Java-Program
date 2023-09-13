bf8e4e4fc7fa55d905d9b941c05c36580e5853a5
nanawarevishal <nanawarevishal18@gmail.com>
2023-09-13 10:51:02 +0530

    HashSet

---
+++ 19.Collection/collection/Set/HashSet/1_hashSet.java
@@ -0,0 +1,19 @@
+package collection.Set.HashSet;
+
+import java.util.HashSet;
+
+class Hashset{
+
+    public static void main(String[] args) {
+        
+        HashSet hs = new HashSet();
+        hs.add("Kanha");
+        hs.add("Rahul");
+        hs.add("Ashish");
+        hs.add("Badhe");
+        hs.add("Rahul");
+        hs.add("Ashish");
+
+        System.out.println(hs);
+    }
+}
