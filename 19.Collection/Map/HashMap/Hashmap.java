e729a19c06f3acca354e828bebc20155628f9b0a
nanawarevishal <nanawarevishal18@gmail.com>
2023-09-14 17:27:08 +0530

    HashMap

---
+++ 19.Collection/Map/HashMap/Hashmap.java
@@ -0,0 +1,19 @@
+package Map.HashMap;
+
+import java.util.HashMap;
+
+class hashmap{
+
+    public static void main(String[] args) {
+
+        HashMap hm = new HashMap();
+        
+        hm.put("Kanha", "Infosys");
+        hm.put("Ashish", "Barclays");
+        hm.put("Badhe", "CarPro");
+        hm.put("Rahul", "BMC");
+
+        System.out.println(hm);
+
+    }
+}
