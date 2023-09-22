4478541a39be110542a572ac68f9b791ee237ef4
nanawarevishal <nanawarevishal18@gmail.com>
2023-09-22 08:15:49 +0530

    ArrayList

--- 19.Collection/collection/list/arrayList/6_UserDefinedSort.java
+++ 19.Collection/collection/list/arrayList/6_UserDefinedSort.java
@@ -1,8 +1,6 @@
 package collection.list.arrayList;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
+import java.util.*;
 
 class Employee{
 
--- 19.Collection/collection/list/arrayList/7_MoviesSort.java
+++ 19.Collection/collection/list/arrayList/7_MoviesSort.java
@@ -1,76 +1,69 @@
 package collection.list.arrayList;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
+import java.util.*;
 
-class Movies{
+class Movies {
+	
+	String name = null;
+	double col = 0.0;
+	float imdb = 0.0f;
 
-    String movieName = null;
-    float IMDBRating = 0.0f;
-    double totColl = 0.0;
-
-    Movies(String movieName,float IMDBRating, double totColl){
-        this.movieName = movieName;
-        this.IMDBRating = IMDBRating;
-        this.totColl = totColl;
-
-    }
-
-    public String toString(){
-
-        return "{ Movie Name: "+movieName+", IMDB Ratings: "+IMDBRating+", Total Collection: "+totColl+" }\n";
-    }
-
-
+	Movies(String name, double col, float imdb) {
+		
+		this.name = name;
+		this.col = col;
+		this.imdb = imdb;
+	}
+	public String toString() {
+		
+		return "{" + name + ":" + col + ":" + imdb + "}";
+	}
 }
 
-
-class SortByMovieName implements Comparator{
-
-    public int compare(Object obj1,Object obj2){
-        return ((Movies)obj1).movieName.compareTo(((Movies)obj2).movieName);
-    }
+class SortedByName implements Comparator {
+	
+	public int compare(Object obj1, Object obj2) {
+		
+		return (((Movies)obj1).name).compareTo(((Movies)obj2).name);
+	}
 }
 
-class SortByMovieCollection implements Comparator{
+class SortedByCol implements Comparator {
 
-    public int compare(Object obj1,Object obj2){
-        return -(int)(((Movies)obj1).totColl - ((Movies)obj2).totColl);
-    }
+	public int compare(Object obj1, Object obj2) {
+	
+		return (int)((((Movies)obj1).col) - (((Movies)obj2).col));
+	}
 }
 
-// class SortByIMDBRating implements Comparator{
+class SortedByIMDB implements Comparator {
 
-//     public int compare(Object obj1,Object obj2){
-//         return (int)(((Movies)obj1).IMDBRating - ((Movies)obj2).IMDBRating);
-//     }
-// }
+	public int compare(Object obj1, Object obj2) {
 
-class SortByIMDBRating implements Comparator<Movies>{
-
-    public int compare(Movies obj1,Movies obj2){
-        return (int)(((obj1).IMDBRating) - ((obj2).IMDBRating));
-    }
+		return (int)((((Movies)obj1).imdb) - (((Movies)obj2).imdb));
+	}
 }
 
-class Client7{
+class Main7 {
 
-    public static void main(String[] args) {
-        
-        ArrayList<Movies> ls = new ArrayList<>();
+	public static void main(String[] args) {
+		
+		ArrayList al = new ArrayList();
 
-        ls.add(new Movies("Iron Man", 10.00f, 25000.00));
-        ls.add(new Movies("Avengers Infinity War", 9.8f, 155000.00));
-        ls.add(new Movies("Avengers Endgame", 9.9f, 455000.00));
-        ls.add(new Movies("Iron Man 3", 9.6f, 89000.00));
+		al.add(new Movies("OMG2", 120.00, 8.0f));
+		al.add(new Movies("Gadar2", 450.00, 9.0f));
+		al.add(new Movies("Kantara", 170.00, 8.7f));
+		al.add(new Movies("Jailer", 250.00, 9.2f));
 
-        System.out.println(ls);
+		System.out.println(al);
 
-        // Collections.sort(ls,new SortByMovieName());
-        Collections.sort(ls,new SortByIMDBRating());
-        // Collections.sort(ls,new SortByMovieCollection());
-
-        System.out.println(ls);
-    }
+		Collections.sort(al, new SortedByName());
+		System.out.println(al);
+		
+		Collections.sort(al, new SortedByCol());
+		System.out.println(al);
+		
+		Collections.sort(al, new SortedByIMDB());
+		System.out.println(al);
+	}
 }
