2fc59273f31e9f2410c00f588420b7e8b61b31d0
nanawarevishal <nanawarevishal18@gmail.com>
2023-09-26 23:13:16 +0530

    File Handling

--- 21.FileHandling/File/JavaFileHandling/5_FileMethods.java
+++ 21.FileHandling/File/JavaFileHandling/5_FileMethods.java
@@ -1,4 +1,4 @@
-package JavaFileHandling;
+package File.JavaFileHandling;
 
 import java.io.*;
 
---
+++ 21.FileHandling/File/ReadWriteFile/1_FileHandDemo.java
@@ -0,0 +1,15 @@
+package File.ReadWriteFile;
+
+
+import java.io.*;
+class FileHandDemo{
+
+    public static void main(String[] args)throws IOException {
+        
+        File f = new File("Icubator.txt");
+        
+        f.createNewFile();
+
+    }
+
+}
