5d39866d1fc09272a40efd003136a2790298b6cc
nanawarevishal <nanawarevishal18@gmail.com>
2023-10-13 19:12:30 +0530

    File Handling

--- 21.FileHandling/File/ReadWriteFile/1_FileHandDemo.java
+++ 21.FileHandling/File/ReadWriteFile/1_FileHandDemo.java
@@ -7,7 +7,7 @@ class FileHandDemo{
     public static void main(String[] args)throws IOException {
         
         File f = new File("Icubator.txt");
-        
+
         f.createNewFile();
 
     }
